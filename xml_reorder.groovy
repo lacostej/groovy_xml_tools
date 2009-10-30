a1e2a90a367ff911e36374cac3763df22588edd2
jerome.lacoste <jerome.lacoste@gmail.com>
2009-10-30 14:20:25 +0100

    fix xml_reorder after refactoring

--- xml_reorder.groovy
+++ xml_reorder.groovy
@@ -6,8 +6,8 @@ inputFilename = this.args[1]
 def result = null
 
 if (modelFilename.endsWith(".groovy"))
-  result = reorderXmlWithGroovyModel(new File(modelFilename), new File(inputFilename))
+  result = transform_functions.reorderXmlWithGroovyModel(new File(modelFilename), new File(inputFilename))
 else
-  result = reorderXml(new File(modelFilename), new File(inputFilename))
+  result = transform_functions.reorderXml(new File(modelFilename), new File(inputFilename))
 
 print result
